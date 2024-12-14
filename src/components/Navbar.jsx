2ec22467328809df06325ea15a4b2fb89d696fc2
FurqanIshaq29 <muhammadfurqanishaq0@gmail.com>
2024-12-14 14:54:33 +0500

    Add to Cart button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
     const [visible,setVisible] = useState(false)
 
-    const {setShowSearch} = useContext(ShopContext)
+    const {setShowSearch , getCartCount} = useContext(ShopContext)
 
     // making the nav bar 
   return (
@@ -62,7 +62,7 @@ const Navbar = () => {
             {/* Cart icon and number of count */}
             <Link to='/cart' className='relative'>
                 <img src={assets.cart_icon} className='w-5 min-w-5' alt="cart" />
-                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4  bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4  bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
             </Link>
 
             {/* Responsive menu icon */}
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,7 +8,7 @@ const Product = () => {
 
   // for product showing by clicking
   const {productId} = useParams();
-  const {products, currency} = useContext(ShopContext);
+  const {products, currency,addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image,setImage] = useState('')
   const [size,setSize] = useState('')
@@ -31,7 +31,6 @@ const Product = () => {
   },[productId,products])
 
 
-  console.log(productId);
   return productData ? (
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
       {/* Product Data */}
@@ -73,7 +72,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <button onClick={() => addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
           <hr className='mt-8 sm:w-4/5' />
           <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
             <p>100% Original product.</p>
