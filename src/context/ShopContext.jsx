f6b5d3be90aeca6041f591bf62460527c7dd7ca9
FurqanIshaq29 <muhammadfurqanishaq0@gmail.com>
2024-12-14 17:43:31 +0500

    Order Information

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import Product from "../pages/Product";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -12,6 +13,8 @@ const ShopContextProvider = (props) => {
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
+    const navigate = useNavigate();
+
 
     // Add to Cart Functionality
     const addToCart = async (itemId,size) => {
@@ -96,7 +99,8 @@ const ShopContextProvider = (props) => {
         cartItems,addToCart,
         getCartCount,
         updateQuantity,
-        getCartAmount
+        getCartAmount,
+        navigate
     }
 
     return (
